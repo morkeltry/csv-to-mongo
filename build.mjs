6d9b50d712f6a88eaf594902fbba8b5148993207
@morkeltry <mork@morksrus.eth>
2021-04-06 15:46:54 +0100

    deals with headers

--- build.mjs
+++ build.mjs
@@ -7,14 +7,24 @@ import { connect } from './index.mjs';
 const source= 'input';
 const input = `CSV/${source}.csv`;
 
-const importCsv = async (filename, db)=> {
+// ignoreRows should be >= headerRows (but both are optional)
+// filterOutliers is a function used for testing for outliers at the CSV import stage, which should populate some external state if its resultsa re to be retained
+const importCsv = async (filename, db, options={} )=> {
+  const { headerRows, ignoreRows, ignorelastRows=0, filterOutliers } = options;
+
   const content = fs.readFileSync(filename);
   const records = csvParseSync(content);
-  return records  
+  const headers = records.splice(0,(ignoreRows || headerRows)).slice(0, headerRows)
+  return {
+    headers,
+    records: records.slice(0, records.length-ignorelastRows)
+  }
 }
 
 const db = await connect();
-const records = await importCsv(input, db)
-console.log(records);
+const { headers, records } = await importCsv(input, db,
+  { 
+  }
+)
 
 db.close();
